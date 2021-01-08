a07a8ad2f3b269339bfc7234685712ae5443227f
Chad Ellison <chad.ellison0123@gmail.com>
2021-01-08 13:57:33 -0700

    add logging back

--- app/channels/video_channel.rb
+++ app/channels/video_channel.rb
@@ -10,9 +10,9 @@ class VideoChannel < ApplicationCable::Channel
     File.open(Rails.root.to_s + '/tmp/image.jpeg', 'wb') do|f|
       f.write(Base64.decode64(opts['imageData'].split(',')[1]), '')
     end
-    # start_time = Time.now
+    start_time = Time.now
     image = RTesseract.new(Rails.root.to_s + '/tmp/image.jpeg', lang: 'eng')
-    # puts "TOTAL tesseract time: ********** #{Time.now - start_time}"
+    puts "TOTAL tesseract time: ********** #{Time.now - start_time}"
     ImageEventBroadcastJob.perform_later({text: image.to_s})
   end
 end
